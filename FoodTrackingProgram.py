73611f07f9c02b78d3b3e72b5eeb6de4067111ee
Evan.van <evan.van@ca2062088w2.ey.net>
2018-11-21 15:24:54 -0500

    FoodTrackingProgram.py

---
+++ FoodTrackingProgram.py
@@ -0,0 +1,22 @@
+import tkinter as Food Tracking Program
+
+def submit():
+	print("Submit pressed")
+	list.append(ent.get())
+	print(list)
+	lab.config(text = "Food Item")
+
+list = []
+
+root = tk.Tk()
+
+lab = tk.Label(root, text = "Input Food")
+lab.pack()
+
+ent = tk.Entry(root)
+ent.pack()
+
+btn = tk.Button(root, text = "Submit", command = submit)
+btn.pack()
+
+root.mainloop()
